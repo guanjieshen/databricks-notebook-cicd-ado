006499ea203a5452cc29fa20e4796818da49dc0b
Guanjie Shen <guanjie.shen@databricks.com>
2023-03-22 20:40:10 -0600

    chore add unit tests

--- notebooks/batch_job.py
+++ notebooks/batch_job.py
@@ -19,19 +19,16 @@ display(df)
 from utils.transformers import convertTimestampToDate, addETLMetadata
 
 
-transformed_df = (df
-                  .transform(lambda df: convertTimestampToDate(df, "birthDate"))
-                  .transform(lambda df: addETLMetadata(df, source))
-                 )
+transformed_df = df.transform(
+    lambda df: convertTimestampToDate(df, "birthDate")
+).transform(lambda df: addETLMetadata(df, source))
 
 # COMMAND ----------
 
 display(transformed_df)
 
 # COMMAND ----------
-
 print(transformed_df.count())
 
 # COMMAND ----------
-
 # MAGIC %md ##### 3. Write to Data Lake
